6365845ce07ad986705b4856c21016f4aba7936c
EC2 Default User <ec2-user@ip-172-31-24-244.ap-northeast-1.compute.internal>
2022-05-10 10:13:45 +0000

    “管理者属性の追加”

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220507005748) do
+ActiveRecord::Schema.define(version: 20220510081658) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20220507005748) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
-    t.string "priv_type"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,18 +4,18 @@ User.create!( name: "管理者",
               email: "sample@email.com",
               password: "password",
               password_confirmation: "password",
-              priv_type: "管理者")
+              admin: true)
               
 100.times do |n|
   name  = Faker::Name.name
   email = "sample-#{n+1}@email.com"
   password = "password"
-  priv_type = "一般"
+  admin = "false" 
   User.create!(name: name,
                email: email,
                password: password,
                password_confirmation: password,
-               priv_type: "一般")
+               admin: false)
 end
 
 users = User.order(:created_at).take(3)
